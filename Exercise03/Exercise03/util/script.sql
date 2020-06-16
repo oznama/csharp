74249a6abe049da6e8f3d17f697043afca88d533
oziel.naranjo@mystratis.com-at-198200525576 <oziel.naranjo@mystratis.com>
2020-06-15 21:43:50 -0500

    Agregando script de la base de datos con algunos querys de ejemplo

---
+++ Exercise03/Exercise03/util/script.sql
@@ -0,0 +1,14 @@
+﻿create table products(
+	id int not null identity,
+	name varchar(20) not null,
+	description varchar(50) not null,
+	short_name nvarchar(10) not null,
+	price decimal not null,
+	stack int not null
+	primary key(id)
+)
+
+INSERT INTO products (name, description, short_name, price, stack)
+VALUES ('Chetos', 'Chetos clasicos con queso', 'CHETOS', 10.00, 20);
+
+SELECT * FROM products;
