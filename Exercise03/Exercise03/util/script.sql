d52f8b9c6d087860d4892a32533318467e9d49b7
oziel.naranjo@mystratis.com-at-198200525576 <oziel.naranjo@mystratis.com>
2020-06-19 20:38:04 -0500

    Agregando script version 2 de la base de datos

--- Exercise03/Exercise03/util/script.sql
+++ Exercise03/Exercise03/util/script.sql
@@ -8,6 +8,78 @@
 	primary key(id)
 )
 
+create table users(
+	id int not null identity,
+	username varchar(20) not null,
+	pswd varchar(20) not null,
+	fullname varchar(100) not null,
+	primary key(id)
+)
+
+create table providers(
+	id int not null identity,
+	name varchar(50) not null,
+	description varchar(50) not null,
+	created_date datetime not null default getdate(),
+	user_id int not null,
+	primary key(id),
+	foreign key (user_id) references users(id)
+)
+
+create table purchases(
+	id int not null identity,
+	purchase_date datetime not null default getdate(),
+	user_id int not null,
+	provider_id int not null,
+	purchase_total decimal not null,
+	primary key(id),
+	foreign key (user_id) references users(id),
+	foreign key (provider_id) references providers(id)
+)
+
+create table purchases_item(
+	id int not null identity,
+	product_id int not null,
+	purchase_id int not null,
+	quantity int not null,
+	primary key(id),
+	foreign key (product_id) references products(id),
+	foreign key (purchase_id) references purchases(id)
+)
+
+create table clients(
+	id int not null identity,
+	name varchar(100) not null,
+	address varchar(100),
+	phone varchar(10),
+	user_id int not null,
+	created_date datetime not null default getdate(),
+	primary key(id),
+	foreign key (user_id) references users(id)
+)
+
+create table sales(
+	id int not null identity,
+	sale_date datetime not null default getdate(),
+	user_id int not null,
+	sale_total decimal not null,
+	client_id int,
+	trusted bit default 0,
+	primary key(id),
+	foreign key (user_id) references users(id),
+	foreign key(client_id) references clients(id)
+)
+
+create table sales_item(
+	id int not null identity,
+	product_id int not null,
+	sale_id int not null,
+	quantity int not null,
+	primary key(id),
+	foreign key (product_id) references products(id),
+	foreign key (sale_id) references sales(id)
+)
+
 
 INSERT INTO products (name, description, short_name, price, stack)
 VALUES ('Chetos', 'Chetos clasicos con queso', 'CHETOS', 10.00, 20);
