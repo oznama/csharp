67b6ddb477d36a9b5436dd2c3eb8fcb4cde24326
Oziel Naranjo <oznama27@gmail.com>
2020-07-13 23:29:28 -0500

    Agregando tabla 2 en script

--- EntityFramework/queries/script.sql
+++ EntityFramework/queries/script.sql
@@ -2,9 +2,18 @@ create database dbentfram;
 
 use dbentfram;
 
+
 create table table01(
 	id int not null identity,
 	name varchar(25) not null,
 	description varchar(25) not null,
+	primary key(id)
+)
+
+create table table02(
+	id int not null identity,
+	name varchar(25) not null,
+	description varchar(25) not null,
+	created datetime not null default getdate(),
 	primary key(id)
 )
